a76f52a6051e32a32760ebfe4afcf44574ed2763
lakshayseth <lakshayseth@yahoo.com>
2016-10-10 09:15:35 -0700

    Updated Database_connector.py

--- Database_connector.py
+++ Database_connector.py
@@ -1,17 +1,23 @@
 import MySQLdb
-#MySQL host: rosencrantz.berkeley.edu
-#user: uspto
-#password: ferrisbueller
-#port: 3306
-connection = MySQLdb.connect(
-                host = 'rosencrantz.berkeley.edu',
-                user = 'uspto',
-                passwd = 'ferrisbueller')  # create the connection
+def connection(organization, patent_id):
+    try:
+        connection = MySQLdb.connect(
+            host = 'rosencrantz.berkeley.edu',
+            user = 'uspto',
+            passwd = 'ferrisbueller')  #Creates the connection
+        #return "Success"
+        cursor = connection.cursor() 
+        #Gets the assigne_id
+        cursor.execute("""SELECT assignee.id, assignee.name_first, assignee.name_last, assignee.organization, patent_assignee.patent_id 
+                        FROM uspto.assignee 
+                        INNER JOIN uspto.patent_assignee ON assignee.id = patent_assignee.assignee_id 
+                        WHERE organization like %s and patent_assignee.patent_id = %s;""", 
+                       ('%'+organization+'%', patent_id,))
+        tables = cursor.fetchall() 
+        if not tables:
+            return "Patent ID does not match with organization."
+        return tables
+    except MySQLdb.Error:
+        return "ERROR IN CONNECTION"
 
-cursor = connection.cursor()     # get the cursor
-#cursor.execute("USE uspto") # select the database
-#cursor.execute("SHOW TABLES")    # execute 'SHOW TABLES' (but data is not returned)
-#cursor.execute("SELECT * FROM uspto.application Where date = '1988-06-23' and patent_id = '4920694'")
-tables = cursor.fetchall() 
-
-print tables
+print connection("Myriad Genetics, Inc", '5693473')
