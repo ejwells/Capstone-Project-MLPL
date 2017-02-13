8ea77a4335316edc0b78a74920dac4e11f7fa62f
titouanjehl <titouan.jehl@gmail.com>
2017-02-12 18:24:51 -0800

    bug fixing tuple evaluation

--- evaluation.py
+++ evaluation.py
@@ -6,12 +6,23 @@ def evaluation_citation(root,ranked_list):
     input: root of the tree, ranked list of the most similar patents to that root
     output: list of tuples patent no, rank
     '''
+    if not isinstance(root, int):
+        raise TypeError
+    if not isinstance(ranked_list,list):
+        raise TypeError
+    if not isinstance(ranked_list[0],int):
+        raise TypeError
+    if len(str(root))!=7:
+        raise ValueError
+    if len(str(ranked_list[0]))!=7:
+        raise ValueError
+
     citations=gg.get_patent_citation(root)
     output=[]
     for patent in citations:
         try:
             idx=ranked_list.index(patent)
-            output.append([patent,idx])
+            output.append((patent,idx))
         except:
-            output.append([patent,-1])
+            output.append((patent,-1))
     return output
