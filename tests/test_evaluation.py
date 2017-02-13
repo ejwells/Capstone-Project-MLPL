b734f0f2740a6900d20dbd38dd106858feaf8cc8
titouanjehl <titouan.jehl@gmail.com>
2017-02-12 18:24:26 -0800

    some new tests for evaluation

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -7,13 +7,22 @@ import numpy as np
 
 class TestEvaluation( unittest.TestCase ):
 
+    def test_input_evaluation(self):
+        root=8111141
+        ranked_list=[5170172, 5631642, 5977913, 6542114, 6720888, 6765484, 6940392, 7248883, 7751957, 2007027]
+        str_list=[str(root)]
+        self.assertRaises(TypeError,evaluation.evaluation_citation,str(root),ranked_list)
+        self.assertRaises(TypeError,evaluation.evaluation_citation,root,str_list)
+        self.assertRaises(ValueError,evaluation.evaluation_citation,1,ranked_list)
+        self.assertRaises(ValueError,evaluation.evaluation_citation,root,[1])
+
     def test_patent_absent(self):
         root=8111141
         ranked_list=[8111141]
         true_res=[5170172, 5631642, 5977913, 6542114, 6720888, 6765484, 6940392, 7248883, 7751957, 2007027]
         exp_result=[]
         for elt in true_res:
-            exp_result.append([elt,-1])
+            exp_result.append((elt,-1))
         a=np.array(evaluation.evaluation_citation(root,ranked_list))
         b=np.array(exp_result)
         np.testing.assert_array_almost_equal(a, b, decimal=6)
@@ -24,7 +33,7 @@ class TestEvaluation( unittest.TestCase ):
         ranked_list=[5170172, 5631642, 5977913, 6542114, 6720888, 6765484, 6940392, 7248883, 7751957, 2007027]
         exp_result=[]
         for elt in ranked_list:
-            exp_result.append([elt,ranked_list.index(elt)])
+            exp_result.append((elt,ranked_list.index(elt)))
         a=np.array(evaluation.evaluation_citation(root,ranked_list))
         b=np.array(exp_result)
         np.testing.assert_array_almost_equal(a, b, decimal=6)
