8b074e433ad0002f632e646520221be75777847d
titouanjehl <titouan.jehl@gmail.com>
2017-02-12 16:50:45 -0800

    some tests

---
+++ tests/test_evaluation.py
@@ -0,0 +1,30 @@
+import unittest
+import sys, os
+# add source directory to path
+sys.path.append( os.path.abspath( '..' ))
+import evaluation
+import numpy as np
+
+class TestEvaluation( unittest.TestCase ):
+
+    def test_patent_absent(self):
+        root=8111141
+        ranked_list=[8111141]
+        true_res=[5170172, 5631642, 5977913, 6542114, 6720888, 6765484, 6940392, 7248883, 7751957, 2007027]
+        exp_result=[]
+        for elt in true_res:
+            exp_result.append([elt,-1])
+        a=np.array(evaluation.evaluation_citation(root,ranked_list))
+        b=np.array(exp_result)
+        np.testing.assert_array_almost_equal(a, b, decimal=6)
+        pass
+
+    def test_evaluation(self):
+        root=8111141
+        ranked_list=[5170172, 5631642, 5977913, 6542114, 6720888, 6765484, 6940392, 7248883, 7751957, 2007027]
+        exp_result=[]
+        for elt in ranked_list:
+            exp_result.append([elt,ranked_list.index(elt)])
+        a=np.array(evaluation.evaluation_citation(root,ranked_list))
+        b=np.array(exp_result)
+        np.testing.assert_array_almost_equal(a, b, decimal=6)
